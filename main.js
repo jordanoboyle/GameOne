86180d667cd17203ddf4862f279f025aa38e9630
jordanoboyle <jmcmcode@gmail.com>
2024-06-03 21:35:27 -0600

    game scene rendered with proper origin

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css';
-import Phaser from 'phaser';
+import Phaser, { Physics } from 'phaser';
 
 
 //dries up the code for canvas sizes.
@@ -8,11 +8,35 @@ const sizes = {
   height:500
 };
 
+const speedDown = 300;
+
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super("scene-game");
+  }
+
+
+  preload() {
+    this.load.image("bg", "/assets/bg.png");
+  }
+  create() {
+    this.add.image(0, 0, "bg").setOrigin(0, 0);
+  }
+  update() {}
+}
+
 const config = {
   type:Phaser.WEBGL,
   width:sizes.width,
   height:sizes.height,
   canvas:gameCanvas,
+  physics: {
+    default:"arcade",
+    arcade: {
+      gravity: {y:speedDown},
+    }
+  },
+  scene:[GameScene]
 };
 
 const game = new Phaser.Game(config);

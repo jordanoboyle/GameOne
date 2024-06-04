c01354bfcfaaaa8847f49f13103393c0794ccb00
jordanoboyle <jmcmcode@gmail.com>
2024-06-04 15:12:53 -0600

    added physics to basket, finishing player movement

--- main.js
+++ main.js
@@ -13,6 +13,9 @@ const speedDown = 300;
 class GameScene extends Phaser.Scene {
   constructor() {
     super("scene-game");
+    this.player;  //essence of OOP in basic games
+    this.cursor; //passed to create() and then movement defined
+    this.playerSpeed = speedDown + 50; //defines how fast player can go
   }
 
 
@@ -22,9 +25,23 @@ class GameScene extends Phaser.Scene {
   }
   create() {
     this.add.image(0, 0, "bg").setOrigin(0, 0);
-    this.add.image(210, 420, "basket").setOrigin(0, 0); 
+    this.player = this.physics.add.image(0, sizes.height - 100, "basket").setOrigin(0, 0); // size of canvas - 100, declare physics before .add
+    this.player.setImmovable(true); //imparts the ability of body/sprite to not be knocked around by other sprites (turn on to see effects)
+
+    this.player.body.allowGravity = false; //takes away effect of gravity from line 42
+
+    this.cursor = this.input.keyboard.createCursorKeys();  //here we are assigning movement to the keys. 
+
   }
-  update() {}
+  update() {
+    //Below we are going to define cursoe movement of the player, passing the player from the constructor
+    const { left, right } = this.cursor; 
+    // logic below defines player movement with conditional
+    if (left.isDown) {
+      this.player.setVelocityX(-this.playerSpeed);
+    } else if (right.isDown)
+
+  }
 }
 
 const config = {

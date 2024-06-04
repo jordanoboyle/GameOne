c514a51fc4cf130759f20393c9522d3ac3233f27
jordanoboyle <jmcmcode@gmail.com>
2024-06-03 21:09:45 -0600

    canvas configurations, dried sizes code

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 import './style.css';
+import Phaser from 'phaser';
+
+
+//dries up the code for canvas sizes.
+const sizes = {
+  width:500,
+  height:500
+};
+
+const config = {
+  type:Phaser.WEBGL,
+  width:sizes.width,
+  height:sizes.height,
+  canvas:gameCanvas,
+};
+
+const game = new Phaser.Game(config);
 

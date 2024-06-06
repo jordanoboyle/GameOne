ac490964b8176a220df31ca0f660ae189ef38f29
jordanoboyle <jmcmcode@gmail.com>
2024-06-06 14:35:21 -0600

    cursor movement, setCollideWorldBounds (no movement off screen), add basic apple sprite

--- main.js
+++ main.js
@@ -16,26 +16,37 @@ class GameScene extends Phaser.Scene {
     this.player;  //essence of OOP in basic games
     this.cursor; //passed to create() and then movement defined
     this.playerSpeed = speedDown + 50; //defines how fast player can go
+
+    //loading the target apple, playeer sprite acquire
+    this.target;
   }
 
 
   preload() {
     this.load.image("bg", "/assets/bg.png");
     this.load.image("basket", "/assets/basket.png");
+    this.load.image("apple", "/assets/apple.png");
   }
   create() {
+    //SCENE 
     this.add.image(0, 0, "bg").setOrigin(0, 0);
+    
+    //PLAYER SPRITES AND PHYSICS
     this.player = this.physics.add.image(0, sizes.height - 100, "basket").setOrigin(0, 0); // size of canvas - 100, declare physics before .add
     this.player.setImmovable(true); //imparts the ability of body/sprite to not be knocked around by other sprites (turn on to see effects)
-
     this.player.body.allowGravity = false; //takes away effect of gravity from line 42
-
+    this.player.setCollideWorldBounds(true);  //Phaser library for specifics, but it applies collision physics to player (scene specific)
     this.cursor = this.input.keyboard.createCursorKeys();  //here we are assigning movement to the keys. 
+
+    //TARGET SPRITES
+    this.target = this.add.image(200, sizes.height - 400, "apple").setOrigin(0,0);
+
+
 
   }
   update() {
-    //Below we are going to define cursoe movement of the player, passing the player from the constructor
-    const { left, right } = this.cursor; 
+    //Below we are going to define curser movement of the player, passing the player from the constructor
+    const { left, right } = this.cursor; //restricting the cursor to key strokes left and right
     // logic below defines player movement with conditional
     if (left.isDown) {
       this.player.setVelocityX(-this.playerSpeed);

ecb998592916bd4ceee1259a90e8e13396a4a19c
jordanoboyle <jmcmcode@gmail.com>
2024-06-07 19:21:44 -0600

    line 54, collision player target

--- main.js
+++ main.js
@@ -19,6 +19,10 @@ class GameScene extends Phaser.Scene {
 
     //loading the target apple, playeer sprite acquire
     this.target;
+
+    //We need a variable to handle collisions and points
+    this.points = 0;
+
   }
 
 
@@ -38,26 +42,27 @@ class GameScene extends Phaser.Scene {
     this.player.setCollideWorldBounds(true);  //Phaser library for specifics, but it applies collision physics to player (scene specific)
     this.cursor = this.input.keyboard.createCursorKeys();  //here we are assigning movement to the keys. 
 
+    
     //TARGET SPRITES
     //this set up allows for readability, adjustment
     this.target = this.physics.add
       .image(200, sizes.height - 400, "apple")
       .setOrigin(0,0)
-      .setMaxVelocity(0, speedDown + 25)
-
-
-
+      .setMaxVelocity(0, speedDown + 50); //stops the target from "falling" continuously faster
+    
+    //This get's technical even for a simple game. Refer to notes for in depth: also hase to come after .target being defined
+    this.physics.add.overlap(this.target, this.player, this.targetHit, null, this);
   }
+
+
   update() {
     //APPLE LOGIC CONTROLLING SPEED
     if (this.target.y >= sizes.height) {
       this.target.setY(0);
+      this.target.setX(this.getRandomX()); //here we are saying pick a random number on Xaxis to return to
     }
-
-    getRandomX() {
-      
-    }
-
+    
+    
     //PLAYER CONTROLS, passing the player from the constructor
     const { left, right, up, down } = this.cursor; //restricting the cursor to key strokes left and right
     // logic below defines player movement with conditional
@@ -68,9 +73,21 @@ class GameScene extends Phaser.Scene {
     } else {
       this.player.setVelocityX(0);
       this.player.setVelocityY(0);
-    }
-
+    }      
   }
+   
+  // must be declared outside the update function, but within the class. 
+  getRandomX() {
+    return Math.floor(Math.random() * 480);
+  } 
+
+  //This function handles collisions between basket and apple...resets Ycoord to 0, calls getRandomX to set Xcoord point
+  targetHit () {
+    this.target.setY(0);
+    this.target.setX(this.getRandomX()); //when hit, recycle apple to top of screen
+    this.points++; //Does this look familiar?
+  }
+
 }
 
 const config = {
